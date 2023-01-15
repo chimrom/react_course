21feff84869a2fe6e02b87bf8434328943a3bfa3
chimrom <116731372+chimrom@users.noreply.github.com>
2023-01-15 18:47:31 +0300

    1st Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useState} from "react";
+import PostList from "./Components/PostList/PostList";
+import MyButton from "./Components/UI/button/MyButton";
+import './Styles/App.css'
 
 function App() {
+  const[posts, setPosts] = useState( [
+    {id: 1, title: 'JavaScript 1', body: 'Description'},
+    {id: 2, title: 'JavaScript 2', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'}
+  ])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+          <input type="text" placeholder="Название поста"/>
+          <input type="text" placeholder="Описание поста"/>
+          <MyButton>Add</MyButton>
+      </form>
+      <PostList posts={posts} title={'Список постов 1'}/>
     </div>
   );
 }
